cccd2f01ea98ae19284e8ea592467c0da197a45f
Breeze-ui <69590792+Breeze-ui@users.noreply.github.com>
2021-04-03 14:20:20 +0530

    -v1.0.7 - PORT Update

--- app.js
+++ app.js
@@ -73,6 +73,6 @@ app.post("/confirmation", async (req, res) => {
     res.render("success.ejs", {});
 });
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running is up!");
 });
